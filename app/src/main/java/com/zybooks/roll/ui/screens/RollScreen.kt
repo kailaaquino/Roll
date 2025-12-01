688f8edfd16069e96b8c51a79b1d90e67ab644d2
Kaila Aquino <kailad.aquino@gmail.com>
2025-12-01 10:14:37 -0800

    Add dice image

--- app/src/main/java/com/zybooks/roll/ui/screens/RollScreen.kt
+++ app/src/main/java/com/zybooks/roll/ui/screens/RollScreen.kt
@@ -1,8 +1,14 @@
 package com.zybooks.roll.ui.screens
 
+import com.zybooks.roll.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -11,6 +17,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 
@@ -28,16 +35,28 @@ fun RollScreen() {
         }
     ) { innerPadding ->
 
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding),
-            contentAlignment = Alignment.Center
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "insert dice here",
+                text = "Shake or tap to roll!",
+                style = MaterialTheme.typography.headlineMedium,
                 textAlign = TextAlign.Center
             )
+            Image(
+                painter = painterResource(R.drawable.dice),
+                contentDescription = "Dice",
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .clickable{
+
+                    }
+            )
+
         }
     }
 }
