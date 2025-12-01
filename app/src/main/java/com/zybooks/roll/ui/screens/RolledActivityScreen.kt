67edb4fd244f299e6852bc35a0ea11b55fa33790
Kaila Aquino <kailad.aquino@gmail.com>
2025-12-01 11:25:47 -0800

    Create rolled activity screen

--- app/src/main/java/com/zybooks/roll/ui/screens/RolledActivityScreen.kt
+++ app/src/main/java/com/zybooks/roll/ui/screens/RolledActivityScreen.kt
@@ -1,9 +1,51 @@
 package com.zybooks.roll.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.zybooks.roll.data.model.ActivityItem
+import com.zybooks.roll.ui.components.ActivityDetailCard
+import com.zybooks.roll.viewmodel.DeckViewModel
 
 @Composable
-fun RolledActivityScreen()
-{
+fun RolledActivityScreen(
+    activity: ActivityItem,
+    onGo: () -> Unit,
+    onRollAgain: () -> Unit,
+    viewModel: DeckViewModel
+){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ActivityDetailCard(activity = activity, viewModel = viewModel)
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ){
+            Text("Go")
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ){
+            Text("Roll Again")
+        }
+    }
 
 }
